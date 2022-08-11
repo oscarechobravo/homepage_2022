cab05419ab1dbb39029b475df895ad53f5521fd7
Oliver Bates <oliver.bates@gmail.com>
2022-08-11 06:22:24 +0100

    first upload oh new homepage

---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <Header/>
+    <article>
+      <div id="about" class="fl-100 pl4 pr4">
+        <h1>About</h1>
+        <p>I'm Oliver co-owner/worker of fractals co-op, Research Fellow in the School of Computing and Communications at Lancaster University, and a partner of Cetis LLP.</p>
+      </div>
+
+      <div class="fl-100 pl4 pr4 bg-washed-yellow-yellow">
+        <h2>Systems and practice change at fractals co-op</h2>
+        <p>fractals started when 4 friends got together to help achieve social impacts by guiding organisations and communities through processes that help them challenge the systems and practices. Broadly speaking we work with any organisation who is values alligned and wants to improve themselves, their products/services, or help communities achieve change. Often digital technology is at the core of this work, but it doesn't have to be. Come and talk to us.  </p>
+
+        <h2>Research at Lancaster University</h2>
+        <p>
+          Oliver is a <a href="https://www.lancaster.ac.uk/people-profiles/oliver-bates">researcher and technology designer</a>&nbsp;at Lancaster University who delivers multi-stakeholder projects through mixed-methods approaches. The co-design and development of tech focusing on doing good for people and the environment.
+        </p>
+        <p>I specialise in research projects that bring together human-factors, with social and environmental injustice. Whilst I have qualifications what I love doing is bringing together people and methods that are appropraite for making social and environmental change where digital tech is involved.  I like to use a wide range of methods, often adapting, chopping, changing, and remixing them to fit the projects on which I work.</p>
+        <br/>
+
+        <h2>EduTech and Learning Technologies at Cetis LLP</h2>
+        <p>I joined Cetis in 2018 looking to bring my user research and communication skills out of academia and into industry. </p>
+
+        <p><strong><em>Current research projects:</em></strong></p>
+        <ul>
+          <li><a href="https://wp.lancs.ac.uk/net0i/">Net0Insights</a></li>
+          <li>Meal Deal</li>
+        </ul>
+        <br/>
+        <p><strong>co-owner/worker, fractals co-operative</strong> - Oliver is a co-owner/worker at <a href="https://fractals.coop">fractals co-operative</a>.</p>
+        <p><strong>Partner, Cetis LLP</strong> - Oliver is also an  of&nbsp;<a href="https://www.cetis.org.uk/">Cetis</a>, a technology cooperative focusing on consulting in learning technologies, data analytics and dashboards.</p>
+        <br/>
+      </div>
+      <div class="fl-100 pl4 pr4">
+        <p><strong>MAIN RESEARCH ROLES:</strong>&nbsp;user research, service evaluation, co-design, human centred design (HCI), data science pipelines, data visualisation, data and database management.</p>
+        <p><strong>EXPERTISE AND INTERESTS:</strong>&nbsp;Multi-disciplinary research, qualitative and quantitative methods, grant writing, team leadership, co-design, design workshops, technology prototyping, critical design, report writing, service design, tech for good, environmental and social justice, data science in transport and logistics, digital technology policy, IoT, Big Data, sensor networks, data science, user centred design, Smart Cities, home energy.</p>
+      </div>
+      <div class="fl-100 pl4 pr4">
+        <p><strong>Previous research projects:</strong></p><!-- I'd want to have some interactive thing here, with boxes and projects probably, like album covers, but hover over it and you get something-->
+        <ul>
+          <li>
+            <strong>
+              <a href="https://www.flipgig.org">FlipGig: <em>Transforming last mile logistics in the gig economy to be fairer and more sustainable.</em>
+              </a><em><a href="https://www.flipgig.org">&nbsp;</a></em></strong>
+            EPSRC funded, tackling the growing issues of unfair work and sustainability on last-mile deliveries of food and goods. Working with Unions and workers to empower and protect couriers.
+          </li>
+          <li>
+            <strong><a href="https://not-equal.tech/portfolio/switch-gig/">Switch-gig:&nbsp;</a></strong><em><strong><a href="https://not-equal.tech/portfolio/switch-gig/">Exploring digital opportunities to switch the gig-economy to fair and just work</a></strong></em><strong><a href="https://not-equal.tech/portfolio/switch-gig/">.</a></strong>&nbsp;Not Equal funded (EPSRC Network+) research project exploring workers rights of food delivery riders in the platform gig economy. Cultural probes, workshops, and provocative design.
+          </li>
+          <li>
+            <strong><a href="http://www.ftc2050.com/">Freight Traffic Control 2050 (FTC2050</a></strong><a href="https://www.ftc2050.com/">)</a>&nbsp;EPSRC funded, enabling collaborative and sustainable last mile logistics. I led work on the data science pipeline for transport data, and user research with industry and policy stakeholders and to design new collaborative services for last-mile logistics.
+          </li>
+          <li>
+            <strong>Energy Information System (EIS)</strong>&nbsp;Energy Lancaster: Prototyping and building a data hub for Lancaster University’s energy and building data as a proof of concept for the Lancaster University “Living Laboratory” and Data Science (DSI).
+          </li>
+        </ul>
+        <br/>
+        <p>
+          <strong>TECHNOLOGY USED:</strong>&nbsp;Kanban, Java, Python, C# (.NET), MySQL, MongoDB, CKAN, WordPress, github, gitlab, Linux, SVN, LaTeX, Spark, HTML, CSS, Javascript, PHP, Mac OS, VMWare, QGIS, Node.js, Matlab
+        </p>
+        <br/>
+        <p>
+          Academic Publications: <a href="https://scholar.google.co.uk/citations?user=ZXDbf_EAAAAJ&amp;hl=en">[Google Scholar]</a> <a href="https://www.lancaster.ac.uk/people-profiles/oliver-bates">[Academic Profile]</a>
+        </p>
+      </div>
+
+      <div id="contact" class="fl-100 white-70 pl4 pr4 bg-purple pv4 pv3-ns pl6">
+        <h1>Get In Touch</h1>
+        <p>my socials</p>
+        <p>linkedOut</p>
+        <p>SmallBlueBird</p>
+        <p>LeGram</p>
+      </div>
+    </article>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
